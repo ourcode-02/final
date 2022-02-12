db7cf754c08622d58b02dea24fe10df95af5d8f0
ourcode-02 <96509160+ourcode-02@users.noreply.github.com>
2022-02-12 17:02:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<?php
+$projectname="Choudhary Transport Company";
+$email="ummedsou99@gmail.com";
+$address="G.E. Road,T.p.,Ring RoadNo.1,Near Srona Bridge Tatibandh, Raipur(C.G.)-492099";
+$phone="+91 75097 53259";
+
+
+?>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Choudhary Transport Company</title>
+<!--Bootstrap-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<!--Stylesheets-->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!--Responsive-->
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<!--Animation-->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<!--Prettyphoto-->
+
+<!--Font-Awesome-->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<!--Owl-Slider-->
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+<link rel="stylesheet" type="text/css" href="css/owl.transitions.css">
+<!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+  [endif]-->
+</head>
+<body data-spy="scroll" data-target=".navbar-default" data-offset="100">
+<!--Preloader-->
+<div id="preloader">
+  <div id="pre-status">
+    <div class="preload-placeholder"></div>
+  </div>
+</div>
+<!--Navigation-->
+<header id="menu">
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <a class="navbar-brand" href="#menu"><img src="images/Logo/" alt=""></a>LOGO </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a class="scroll" href="#menu">Home</a></li>
+            <li><a class="scroll" href="#about">About</a></li>
+            <li><a class="scroll" href="#service">Service</a></li>
+            <li><a class="scroll" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+    </div>
+  </div>
+</header>
+<!--Slider-Start-->
+<section id="slider">
+  <div id="home-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="item active" style="background-image:url(images/Slider/03.jpg)">
+        <div class="carousel-caption container">
+          <div class="row">
+            <div class="col-md-7 col-sm-12 col-xs-12">
+              <h1>Choudhary Transport Company</h1>
+              <h2>Safe and Speedy Transport</h2>
+              <p>Cosmix – A One Page Parallax, HTML5 and Responsive Template suitable for any creative business agency. Multiple pages also included in this theme with lots of CSS and JQuery animations</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item" style="background-image:url(images/Slider/04.jpg)">
+        <div class="carousel-caption container">
+          <div class="row">
+            <div class="col-md-7 col-sm-12 col-xs-12">
+              <h1>Choudhary Transport Company</h1>
+              <h2>Safe and Speedy Transport</h2>
+              <p>Cosmix – A One Page Parallax, HTML5 and Responsive Template suitable for any creative business agency. Multiple pages layouts also in this theme with lots of CSS and JQuery animations</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item" style="background-image:url(images/Slider/03.jpg)">
+        <div class="carousel-caption container">
+          <div class="row">
+             <div class="col-md-7 col-sm-12 col-xs-12">
+              <h1>Choudhary Transport Company</h1>
+              <h2>Safe and Speedy Transport</h2>
+              <p>Cosmix – A One Page Parallax, HTML5 and Responsive Template suitable for any creative business agency. Multiple page also included in this theme with lots of CSS and JQuery animations</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="home-carousel-left" href="#home-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a> <a class="home-carousel-right" href="#home-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a> </div>
+  </div>
+  <!--/#home-carousel-->
+</section>
+<!--About-Section-Start-->
+<section id="about">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="heading">
+        <h2 class="about-1">ABOUT US</h2>
+        <div class="line"></div>
+        <p><span><strong></strong></span> </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 ab-sec">
+        <div class="col-md-6">
+          <h3 class="wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms"><span></span></h3>
+          <p><span><strong>L</strong></span>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.Lorem Ipsum is simply dummy text of the printing and typesetting industry. book. </p>
+        </div>
+        <div class="col-md-6 ab-sec-img wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms"><img src="images/Aboutus/01.jpg" alt="" style="width: 417px;"> </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--About-Sec-2-Start-->
+
+<!--Service-Section-Start-->
+<section id="service">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="heading">
+        <h2 class="about-1">OUR SERVICE</h2>
+        <div class="line"></div>
+       
+      </div>
+    </div>
+    <div class="row">
+      <div class="features-sec">
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-line-chart"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">Full Load Services</h5>
+              <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-cubes"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">Transparent Pricing</h5>
+              <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-pie-chart"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">Quick & Easy Portal</h5>
+              <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-bar-chart"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">Advance Booking</h5>
+              <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="400ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-language"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">Live Tracking</h5>
+              <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="500ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-bullseye"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">24/7 Service</h5>
+              <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+      </div>
+    </div>
+      <!--/#portfolio-filter-->
+    </div>
+
+
+<!--Contact-Section-Start-->
+<section id="contact">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="heading">
+        <h2 class="about-1">CONTACTUS</h2><br><br>
+        <div class="line"></div>
+        
+      </div>
+    </div>
+    <div class="text-center">
+      <div class="col-md-6 col-sm-6 contact-sec-1">
+       
+        <ul class="contact-form">
+          <li><i class="fa fa-map-marker"></i>
+            <h5 class="add-part">Address: </h5> <span> <?php echo $address;?></span> 
+          </li>
+          <li><i class="fa fa-envelope"></i>
+            <h5 class="add-part">Mail Us: </h5><a href="#">  <span> <?php echo $email;?></a></span> 
+          </li>
+          <li><i class="fa fa-phone"></i>
+            <h5 class="add-part">Phone: </h5><span> <?php echo $phone;?></span> 
+          </li>
+         
+        </ul>
+      </div>
+      <div class="col-md-6 col-sm-6">
+        <form id="main-contact-form" name="contact-form" method="post" action="#">
+          <div class="row  wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+            <div class="col-sm-6">
+              <div class="form-group">
+                <input type="text" name="name" class="form-control" placeholder="Name" required="required">
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="form-group">
+                <input type="email" name="email" class="form-control" style="border-radius:15px;" placeholder="Email Address" required="required">
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <input type="text" name="subject" class="form-control" placeholder="Subject" required="required">
+          </div>
+          <div class="form-group">
+            <textarea name="message" id="message" class="form-control" rows="4" placeholder="Enter your message" required="required"></textarea>
+          </div>
+          
+		  <center><input type="submit" class="btn-send col-md-12 col-sm-12 col-xs-12" value="submit" name="submit" id="submit"></center>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!--Jquery-->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!--Boostrap-Jquery-->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<!--Preetyphoto-Jquery-->
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<!--NiceScroll-Jquery-->
+<script type="text/javascript" src="js/jquery.nicescroll.js"></script>
+<script type="text/javascript" src="js/waypoints.min.js"></script>
+<!--Isotopes-->
+<script type="text/javascript" src="js/jquery.isotope.js"></script>
+<!--Wow-Jquery-->
+<script type="text/javascript" src="js/wow.js"></script>
+<!--Count-Jquey-->
+<script type="text/javascript" src="js/jquery.countTo.js"></script>
+<script type="text/javascript" src="js/jquery.inview.min.js"></script>
+<!--Owl-Crousels-Jqury-->
+<script type="text/javascript" src="js/owl.carousel.js"></script>
+<!--Main-Scripts-->
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
+
+<!-- Hosting24 Analytics Code -->
+<script type="text/javascript" src="http://stats.hosting24.com/count.php"></script>
+<!-- End Of Analytics Code -->
